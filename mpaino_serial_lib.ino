1653612b95f8400f7adc5ff6d4311b0c7178aab3
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-11-29 04:59:44 +0900

    Update mpaino_serial_lib.ino

--- mpaino_serial_lib.ino
+++ mpaino_serial_lib.ino
@@ -1,16 +1,19 @@
+//90,1000 
+//-60,100
 #include <Servo.h>
 
 Servo myservo;  //servo object
 
 void setup() {
-  Serial.begin(9600);  
-  myservo.attach(11);
+  Serial.begin(115200);  
+  myservo.attach(45); // Second Section of MPAINO PWN block
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     commandProcess(command);
+    Serial.print(command);
   }
 }
 
@@ -19,7 +22,7 @@ void commandProcess(String command) {
   int angle = command.substring(0, command.indexOf(',')).toInt();
   int time = command.substring(command.indexOf(',') + 1).toInt();
 
-  angle = constrain(angle, 0, 180);
+  //angle = constrain(angle, 0, 180);
 
   // Run the servo motor with the parsed values
   runServo(angle, time);
