2759129c153aaf719d23a1656a7031fec537b2db
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-12-20 16:32:32 +0900

    Create serial_servo_uno.ino

---
+++ serial_servo_uno.ino
@@ -0,0 +1,117 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(10); // Attach the servo to digital pin 10
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    processCommand(data);
+  }
+}
+
+void runServo(int angle, int runSpeed) {
+  int mappedSpeed = map(runSpeed, 0, 2000, 0, 180);
+  
+  if (angle >= 0) {
+    for (int i = 0; i <= angle; ++i) {
+      myServo.write(i);
+      delay(mappedSpeed);
+    }
+  } else {
+    for (int i = 0; i >= angle; --i) {
+      myServo.write(i);
+      delay(mappedSpeed);
+    }
+  }
+}
+
+void processCommand(String command) {
+  char separator = ',';
+
+  int firstBracketIndex = command.indexOf('(');
+  int secondBracketIndex = command.indexOf(')', firstBracketIndex + 1);
+
+  if (firstBracketIndex != -1 && secondBracketIndex != -1) {
+    // Extract motor type and SID from the first set of brackets
+    String motorTypeAndSID = command.substring(firstBracketIndex + 1, secondBracketIndex);
+    int spaceIndex = motorTypeAndSID.indexOf(' ');
+
+    if (spaceIndex != -1) {
+      String motorType = motorTypeAndSID.substring(0, spaceIndex);
+      String SID = motorTypeAndSID.substring(spaceIndex + 1);
+
+      Serial.print("SID: ");
+      Serial.println(SID);
+
+      // Relay control logic based on motorType
+      if (motorType == "PUMPMOTOR_OPERATION") {
+        // digitalWrite(driverOut1, HIGH);
+        // iswashing = true;
+      } else if (motorType == "WASHING_OPERATION") {
+        // digitalWrite(driverOut2, HIGH);
+        // iswashing = true;
+      } else if (motorType == "SERVOMOTOR_OPERATION") {
+        // Extract servo angle and run speed from subsequent brackets
+        int index = secondBracketIndex + 1;
+        int nextBracketIndex = command.indexOf('(', index);
+        int endIndex = command.indexOf(')', nextBracketIndex + 1);
+
+        if (nextBracketIndex != -1 && endIndex != -1) {
+          String inputData = command.substring(nextBracketIndex + 1, endIndex);
+
+          int commaIndex = inputData.indexOf(separator);
+          if (commaIndex != -1) {
+            int param1 = inputData.substring(0, commaIndex).toInt();
+            int param2 = inputData.substring(commaIndex + 1).toInt();
+
+            Serial.print(motorType);
+            Serial.print(": Param1: ");
+            Serial.print(param1);
+            Serial.print(", Param2: ");
+            Serial.println(param2);
+
+            runServo(param1, param2);
+          } else {
+            Serial.println("Invalid servo data format");
+          }
+        }
+      } else if (motorType == "STEPPERMOTOR_OPERATION") {
+        // Extract stepper angle and run time from subsequent brackets
+        int index = secondBracketIndex + 1;
+        int nextBracketIndex = command.indexOf('(', index);
+        int endIndex = command.indexOf(')', nextBracketIndex + 1);
+
+        if (nextBracketIndex != -1 && endIndex != -1) {
+          String inputData = command.substring(nextBracketIndex + 1, endIndex);
+
+          int commaIndex = inputData.indexOf(separator);
+          if (commaIndex != -1) {
+            int param1 = inputData.substring(0, commaIndex).toInt();
+            int param2 = inputData.substring(commaIndex + 1).toInt();
+
+            Serial.print(motorType);
+            Serial.print(": Param1: ");
+            Serial.print(param1);
+            Serial.print(", Param2: ");
+            Serial.println(param2);
+
+            // Call your stepper motor function here if needed
+          } else {
+            Serial.println("Invalid stepper data format");
+          }
+        }
+      } else {
+        Serial.println("Unknown motor type");
+      }
+    } else {
+      Serial.println("Invalid motor type and SID format");
+    }
+  } else {
+    Serial.println("Invalid command format");
+  }
+}
